59e78e4539b1939cb4143e56d3572774a1e2004d
nekacurry <anneka498@gmail.com>
2021-07-12 23:28:26 -0500

    timer created

--- src/components/GridBoard.js
+++ src/components/GridBoard.js
@@ -1,17 +1,50 @@
-import React from 'react'
 import GridSquare from './GridSquare'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { shapes } from '../utils'
+import React, { useEffect, useRef } from 'react'
+import { moveDown } from '../actions'
+
 
 
 // Represents a 10 x 18 grid of grid squares
 
 export default function GridBoard(props) {
+
+  const requestRef = useRef()
+  const lastUpdateTimeRef = useRef(0)
+  const progressTimeRef = useRef(0)
+  const dispatch = useDispatch()
+
   const game = useSelector((state) => state.game)
   const { grid, shape, rotation, x, y, isRunning, speed } = game
 
   const block = shapes[shape][rotation]
   const blockColor = shape
+  
+
+  const update = (time) => {
+    requestRef.current = requestAnimationFrame(update)
+    if (!isRunning) {
+        return 
+    }
+    if (!lastUpdateTimeRef.current) {
+        lastUpdateTimeRef.current = time
+    }
+    const deltaTime = time - lastUpdateTimeRef.current
+    progressTimeRef.current += deltaTime
+    if (progressTimeRef.current > speed) {
+        dispatch(moveDown())
+        progressTimeRef.current = 0
+    }
+    lastUpdateTimeRef.current = time
+    
+  } 
+
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(update)
+    return () => cancelAnimationFrame(requestRef.current)
+  }, [isRunning])
+
 
   // map rows
   const gridSquares = grid.map((rowArray, row) => {
@@ -33,7 +66,7 @@ export default function GridBoard(props) {
 
       // Generate a unique key for every block
       const k = row * grid[0].length + col;
-      
+
       // Generate a grid square
       return <GridSquare
               key={k}
